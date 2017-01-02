7b2b82edb5fae4b6308c09f0df03fe7fa15ff1e7
Constantine K <looselyrigorous@gmail.com>
2017-01-02 19:07:01 +0200

    Make educated decision on how to set EDITOR/VISUAL

--- zsh/.zsh/config/00-exports.zsh
+++ zsh/.zsh/config/00-exports.zsh
@@ -1,5 +1,14 @@
-export VISUAL=nvim
-export EDITOR=nvim
+if (( $+commands[nvim] )); then
+	export VISUAL=nvim
+	export EDITOR=nvim
+elif (( $+commands[vim] )); then
+	export VISUAL=vim
+	export EDITOR=vim
+elif (( $+commands[nano] )); then
+	export VISUAL=nano
+	export EDITOR=nano
+fi
+
 export PAGER=less
 
 export CLICOLOR=1
