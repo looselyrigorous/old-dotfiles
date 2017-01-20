094c9b7116298f9eaa00f1710788c879c014eb39
Constantine K <looselyrigorous@gmail.com>
2017-01-20 11:51:06 +0200

    Add git shell aliases

---
+++ zsh/.zsh/config/04-git-aliases.zsh
@@ -0,0 +1,16 @@
+alias gp="git push"
+alias gl="git pull"
+alias gs="git status -sb"
+alias glg="git log --graph --decorate --oneline --abbrev-commit"
+alias gd="git diff"
+alias gds="git diff --staged"
+alias gc="git commit"
+alias gco="git checkout"
+alias ga="git add"
+alias ga="git add -p"
+alias gcl="git clone"
+
+# GitIgnore.io
+function gi() {
+	curl -L -s https://www.gitignore.io/api/$@ ;
+}
