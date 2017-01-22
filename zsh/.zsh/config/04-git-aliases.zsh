baf222bdf52c24ffd0d78fa831bddb1bc7201f65
Constantine K <looselyrigorous@gmail.com>
2017-01-22 12:06:17 +0200

    Add more git aliases

--- zsh/.zsh/config/04-git-aliases.zsh
+++ zsh/.zsh/config/04-git-aliases.zsh
@@ -1,14 +1,23 @@
+alias g="git"
 alias gp="git push"
 alias gl="git pull"
 alias gs="git status -sb"
 alias glg="git log --graph --decorate --oneline --abbrev-commit"
+alias glga="$aliases[glg] --all"
 alias gd="git diff"
-alias gds="git diff --staged"
+alias gds="$aliases[gd] --staged"
 alias gc="git commit"
+alias gcm="$aliases[gc] -m"
 alias gco="git checkout"
 alias ga="git add"
-alias gap="git add -p"
+alias gap="$aliases[ga] -p"
 alias gcl="git clone"
+alias gm="git merge"
+alias gst="git stash"
+
+function grcd() {
+	cd "$(git root)";
+}
 
 # GitIgnore.io
 function gi() {
