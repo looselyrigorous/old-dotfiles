7987e4ea7ed3a9f3dab8bd4917a3239b5f036061
Constantine K <looselyrigorous@gmail.com>
2017-10-20 11:55:30 +0300

    🐚 Remove venvwrapper from the base config

--- zsh/.zsh/config/virtualenvwrapper.zsh
+++
@@ -1,7 +0,0 @@
-# Python virtualenvwrapper
-if [[ -f /usr/local/bin/virtualenvwrapper.sh ]]; then
-	export WORKON_HOME=$HOME/.virtualenvs
-	export PROJECT_HOME=$HOME/Devel
-	source /usr/local/bin/virtualenvwrapper.sh
-	export PIP_REQUIRE_VIRTUALENV="true"
-fi
