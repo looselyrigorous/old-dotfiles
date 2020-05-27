f53b69623bb4f530a2c1568219d5f48df3fd1206
Constantine K <looselyrigorous@gmail.com>
2020-05-27 13:15:57 +0300

    🐚 Remove iterm2 integration

--- zsh/.zsh/config/iterm2.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# iTerm Shell Integration
-if [[ "$OSTYPE" == darwin* ]]; then
-	test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-fi
